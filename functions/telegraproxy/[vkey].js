437f33f9f71af5dffa225151101842d26954626f
Han <1655466387@qq.com>
2024-09-07 13:47:31 +0800

    优化

--- functions/telegraproxy/[vkey].js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet({ request }) {
-	const { url, method, headers, body } = request;
-	const newUrl = new URL(url);
-	return fetch(`https://telegra.ph${newUrl.pathname.replace('telegraproxy', 'file')}`, { method, headers, body });
-}
--- functions/upload.js
+++ functions/upload.js
@@ -1,15 +1,18 @@
 export async function onRequest({ request }) {
-	const { url, method, headers, body } = request;
-	const newUrl = new URL(url);
-	const res = await fetch('https://telegra.ph' + newUrl.pathname + newUrl.search, { method, headers, body });
-	const resText = await res.text();
-	let ResUrl = '';
-	try {
-		ResUrl = `https://${newUrl.hostname}/v2/${JSON.parse(resText)[0].src.replace('/file/', '')}`;
-	} catch (error) {
-		ResUrl = 'Failed';
-	}
-	const newRes = new Response(ResUrl);
-	newRes.headers.set('Access-Control-Allow-Origin', '*');
-	return newRes;
+  const { url, method, headers, body } = request
+  const { file, ...bd } = body
+  const newUrl = new URL(url)
+  console.log(newUrl)
+  const res = await fetch(`https://api.imgur.com/3/upload?client_id=546c25a59c58ad7`, { method, headers, body: { ...bd, image: file } })
+  const resText = await res.json()
+  return resText
+  //   if (!resText.data.success) {
+  //     return 'Failed'
+  //   }
+  //   const newRes = new Response(`https://${newUrl.hostname}/v2/${String(resText.data.link).replace('https://i.imgur.com/', '')}`)
+  //   newRes.headers.set('Access-Control-Allow-Origin', '*')
+  //   return newRes
 }
+
+// const client_id = () => `${Array.from({ length: 15 }, () => Math.random().toString(36).slice(2)).join('')}`
+// console.log(client_id)
